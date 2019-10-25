f38f6e35d2aff1bcb0ba79f49d0f8ff5cea8a975
ljf <285079883@qq.com>
2019-10-25 18:05:51 +0800

    test

--- src/main/java/com/ljf/cwebe/controller/HelloController.java
+++ src/main/java/com/ljf/cwebe/controller/HelloController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @RestController
 public class HelloController {
@@ -24,6 +28,34 @@ public class HelloController {
         DataTest dataTest = dataTestMapper.selectByPrimaryKey(1);
         logger.info(dataTest.getText());
         return "hello world";
+    }
+
+    @RequestMapping("/test/img")
+    public void test(HttpServletRequest request,HttpServletResponse response) throws IOException {
+        //方式一
+//		BufferedImage bi = ImageIO.read(new File("C:/Users/ljf/Pictures/girl.jpg"));
+//
+//		// 设置浏览器不缓存本页
+//		response.setHeader("Cache-Control", "no-cache");
+//		// 设置图片类型
+//		response.setContentType("image/jpg");
+//		// 输出验证码给客户端
+//		ImageIO.write(bi, "JPEG", response.getOutputStream());
+
+        //方式二
+        File image = new File("C:/Users/ljf/Pictures/girl.jpg");
+        FileInputStream fis = new FileInputStream(image);
+        int length = fis.available();
+        byte data[] = new byte[length];
+        response.setContentLength(length);
+        String fileName = image.getName();
+        String fileType = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+        response.setContentType(fileType);
+        fis.read(data);
+        OutputStream os = response.getOutputStream();
+        os.write(data);
+        os.flush();
+        fis.close();
     }
 
 }
